2607f07cd9c393a967a5b312201d47ddd5f671c7
Vviniciuss13 <viniciusfravolini@gmail.com>
2024-12-23 19:43:56 -0300

    default value blade

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -8,7 +8,7 @@ class FornecedorController extends Controller
 {
     public function index(){
         $fornecedores = [
-            0 => ['nome' => 'Fornecedor 1', 'status' => 'N', 'cnpj' => ''],
+            0 => ['nome' => 'Fornecedor 1', 'status' => 'N', 'cnpj' => '0'],
             1 => ['nome' => 'Fornecedor 2', 'status' => 'S'],
         ];
 
