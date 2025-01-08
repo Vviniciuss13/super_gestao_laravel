3f4ba5f946ed8f72c6e41d773c8f5444184075e0
Vviniciuss13 <viniciusfravolini@gmail.com>
2025-01-08 16:39:09 -0300

    motivo contatos pelo foreach

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -8,32 +8,15 @@ use App\Models\SiteContato;
 class ContatoController extends Controller
 {
     public function contato(Request $request){
-        /*
-        echo '<pre>';
-        print_r($request->all());
-        echo '</pre>';
-        echo $request->input('nome');
-        echo '<br>';
-        echo $request->input('email');
-        */
-
-        /*
-        $contato = new SiteContato();
-        $contato->nome = $request->input('nome');
-        $contato->telefone = $request->input('telefone');
-        $contato->email = $request->input('email');
-        $contato->motivo_contato = $request->input('motivo_contato');
-        $contato->mensagem = $request->input('mensagem');
-        */
         
-        //$contato = new SiteContato();
-        //$contato->fill($request->all());
-        //$contato->save();
-        //$contato->create($request->all());
+        $motivo_contatos = [
+            '1' => 'Dúvida',
+            '2' => 'Elogio',
+            '3' => 'Reclamação',
+        ];
         
-        //print_r($contato->getAttributes());
 
-        return view('site.contato');
+        return view('site.contato', ['titulo' => 'contato (teste)', 'motivo_contatos' => $motivo_contatos]);
     }
 
     public function salvar(Request $request){
