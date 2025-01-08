aea66b6769cb4663f462fa4c93cb481a02e95033
Vviniciuss13 <viniciusfravolini@gmail.com>
2025-01-08 16:03:35 -0300

    adicionando min e max como validacao

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -39,11 +39,11 @@ class ContatoController extends Controller
     public function salvar(Request $request){
         //realizar a validação dos dados do formulário
         $request->validate([
-            'nome' => 'required',
+            'nome' => 'required|min:3|max:40', // nomes com no mínimo 3 caracters e no máximo 40
             'telefone' => 'required',
             'email' => 'required',
             'motivo_contato' => 'required',
-            'mensagem' => 'required'
+            'mensagem' => 'required|max:2000'
         ]);
         //SiteContato::create($request->all());
     }
