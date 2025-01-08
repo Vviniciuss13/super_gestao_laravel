2b2af80008907e493b3d05a0be81e743f9e26560
Vviniciuss13 <viniciusfravolini@gmail.com>
2025-01-08 17:36:11 -0300

    validacoes e redirecionamento

--- app/Models/SiteContato.php
+++ app/Models/SiteContato.php
@@ -14,5 +14,5 @@ class SiteContato extends Model
 {
     //    
     use HasFactory;
-    protected $fillable = ['nome', 'email', 'telefone', 'motivo_contato', 'mensagem'];
+    protected $fillable = ['nome', 'email', 'telefone', 'motivo_contatos_id', 'mensagem'];
 }
