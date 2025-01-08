970991f0d2ed503c2be7d9ed46f0fe3f8178a13c
Vviniciuss13 <viniciusfravolini@gmail.com>
2025-01-08 17:01:48 -0300

    usando motivos contatos dinamicamente com o bd

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,6 @@ class DatabaseSeeder extends Seeder
             'email' => 'test@example.com',
         ]);
 
-        $this->call([FornecedorSeeder::class, SiteContatoSeeder::class]);
+        $this->call([FornecedorSeeder::class, SiteContatoSeeder::class, MotivoContatoSeeder::class]);
     }
 }
